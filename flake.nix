31de42601a8ec22cdb2fc292d8a5ee0be6c91494
Soliprem <franci.solidoro@gmail.com>
2024-12-14 15:25:11 +0100

    flake package

--- flake.nix
+++ flake.nix
@@ -3,67 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+        ];
+      };
+
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "my-rust-project";
+        version = "0.1.0";
+        
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
         };
-
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rust-analyzer" ];
-        };
-
-        projectName = "my-rust-project";
-      in {
-        # Development shell
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustToolchain
-            cargo
-            rust-analyzer
-            rustfmt
-            clippy
-          ];
-
-          shellHook = ''
-            echo "Rust development environment"
-            echo "Rust version: $(rustc --version)"
-          '';
-        };
-
-        # Build package
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = projectName;
-          version = "0.1.0";
-          
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-
-          meta = with pkgs.lib; {
-            description = "A simple Rust project";
-            homepage = "https://github.com/jameslzhu/${projectName}";
-            license = licenses.mit;
-            platforms = platforms.unix;
-          };
-        };
-
-        # Optional: default app for running the project
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/${projectName}";
-        };
-      }
-    );
+      };
+    };
 }
